f3d7746a66c5bc8b08b8d4b990e3de4303e9ec5b
Laiba-Tanveerr <libatanveer8@gmail.com>
2026-06-02 23:37:12 +0500

    Initialized repositories

---
+++ backend/src/main/java/com/laiba/backend/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.laiba.backend.repository;
+
+import com.laiba.backend.entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<Users, Long> {
+
+    Optional<Users> findByEmail(String email);
+
+    Optional<Users> findByPhoneNo(String phoneNo);
+}

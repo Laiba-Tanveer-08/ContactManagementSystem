7dec38b8e6e4cc85b8d638c3e2361058ee46f9c2
Laiba-Tanveerr <libatanveer8@gmail.com>
2026-06-03 23:58:32 +0500

    Initialized and updated DTO's

---
+++ backend/src/main/java/com/laiba/backend/dto/ContactRequest.java
@@ -0,0 +1,19 @@
+package com.laiba.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ContactRequest {
+    private String firstName;
+    private String lastName;
+    private String title;
+    private List<ContactInfoRequest> contactInfos;
+}

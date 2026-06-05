72701ca146137c0ecec7cfd8b32c183bc6280e96
Laiba-Tanveerr <libatanveer8@gmail.com>
2026-06-06 03:05:05 +0500

    Added layout

---
+++ frontend/src/components/layout/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Sidebar from './Sidebar';
+import Navbar from './Navbar';
+import './Layout.css';
+
+export default function Layout({ children, onSearch, searchValue, onImportDone }) {
+    return (
+        <div className="layout">
+            <Sidebar onImportDone={onImportDone} />
+            <div className="layout-main">
+                <Navbar onSearch={onSearch} searchValue={searchValue} />
+                <main className="layout-content">{children}</main>
+            </div>
+        </div>
+    );
+}

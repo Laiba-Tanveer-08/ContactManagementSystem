8beaf430427eebc0c1d14bc654df8b74ef672557
Laiba-Tanveerr <libatanveer8@gmail.com>
2026-06-06 14:36:18 +0500

    Handled search for full name

--- backend/src/main/java/com/laiba/backend/repository/ContactRepository.java
+++ backend/src/main/java/com/laiba/backend/repository/ContactRepository.java
@@ -4,6 +4,8 @@ import com.laiba.backend.entity.Contacts;
 import com.laiba.backend.entity.Users;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.domain.Pageable;
 
@@ -11,13 +13,11 @@ import java.util.List;
 
 @Repository
 public interface ContactRepository extends JpaRepository<Contacts, Long> {
-    List<Contacts> findByFirstNameContainingIgnoreCase(String firstName);
-
-    List<Contacts> findByLastNameContainingIgnoreCase(String lastName);
-
-    Page<Contacts> findByUserAndFirstNameContainingIgnoreCaseOrUserAndLastNameContainingIgnoreCase(Users user, String firstName, Users user2, String lastName, Pageable pageable);
-
-    List<Contacts> findByUserUserId(Long userId);
-
     Page<Contacts> findByUser(Users user, Pageable pageable);
+
+   // List<Contacts> findByUserUserId(Long userId);
+
+    @Query("SELECT c FROM Contacts c WHERE c.user = :user AND " + "(LOWER(c.firstName) LIKE LOWER(CONCAT('%', :name, '%')) OR "
+            + "LOWER(c.lastName) LIKE LOWER(CONCAT('%', :name, '%')) OR " + "LOWER(CONCAT(c.firstName, ' ', c.lastName)) LIKE LOWER(CONCAT('%', :name, '%')))")
+    Page<Contacts> searchByName(@Param("user") Users user, @Param("name") String name, Pageable pageable);
 }
